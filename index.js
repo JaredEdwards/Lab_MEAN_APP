7201eb3b85ffe95d26566bdc20a0b4dfc3db69b6
Samuel Clements <sdkclements@gmail.com>
2017-03-21 15:56:55 -0400

    set up index show new delete and update functions in controllers

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const Recipe = require('./db/models.js').Recipe
 const app = express();
 
 
+
 //this needs to be removed once angular replaces it
+app.set("port", process.env.PORT || 3001)
 app.set('view engine', 'hbs')
 app.use(parser.urlencoded({
     extended: true
 }))
-app.use(parser.json())
+app.use(parser.json({ extended: true}))
 
 app.listen(3000, _ => {
     console.log("Express is started on port 3000");
@@ -23,15 +25,13 @@ app.get('/', (req, res) => {
     });
 });
 // index or GET all recipes
-app.get('/recipes', (req, res) => {
+app.get('/api/recipes', (req, res) => {
     Recipe.find({}, (err, recipes) => {
-        res.render('recipes', {
-            recipes
-        });
+        res.json(recipes)
     });
 });
 //POST method to create new
-app.post('/recipes', (req, res) => {
+app.post('/api/recipes', (req, res) => {
     let ingredients = req.body.recipe.ingredients;
     ingredients.trim(' ');
     ingredients.split(' ');
@@ -39,39 +39,37 @@ app.post('/recipes', (req, res) => {
         recipe: req.body.recipe.name,
         ingredients: ingredients
     }
-    Recipe.create(newRecipe, (err, url) => {
-        res.redirect('/recipes');
+    Recipe.create(newRecipe, (err, recipe) => {
+        res.json(recipe);
     });
 });
 
 // needs to be a get request for a 'show' view
-app.get('/recipes/:recipe', (req, res) => {
+app.get('/api/recipes/:recipe', (req, res) => {
     Recipe.findOne({
         recipe: req.params.recipe
     }).then((recipe) => {
-        res.render('recipe', {
-            recipe: recipe
-        });
+        res.json(recipe)
     });
 });
 
 // need to get individual show to work to get this to work
-app.post('/recipes/:recipe', (req, res) => {
+app.post('/api/recipes/:recipe', (req, res) => {
     // console.log(req.body.recipe.ingredients);
     Recipe.findOneAndUpdate({
         recipe: req.body.recipe.name
     }, req.body.recipe, {
         new: true
     }).then((recipe) => {
-        res.redirect(`/recipes/${recipe.recipe}`)
+        res.json(recipe)
     })
 })
 
 //delete individual recipes
-app.get('/recipes/:recipe/delete', (req, res) => {
-    Recipe.remove({
+app.delete('/api/recipes/:recipe', (req, res) => {
+    Recipe.findOneAndRemove({
         recipe: req.params.recipe
     }, () => {
-        res.redirect('/recipes')
+        res.json('/recipes')
     })
 })
