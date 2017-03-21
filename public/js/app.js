8a7e44195f78c0e1205003168c7145a3610bb827
Samuel Clements <sdkclements@gmail.com>
2017-03-21 15:24:11 -0400

    added basic angular setup

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,53 @@ angular
         'ui.router',
         'ngResource'
     ])
+    .config([
+        "$stateProvider",
+        Router
+    ])
+    .factory("Recipe", [
+        "$resource",
+        Recipe
+    ])
+    .controller("indexController", [
+    	"Recipe",
+    	indexControllerFunction
+    	])
+    .controller("showController", [
+    	"$stateParams"
+    	"Recipe",
+    	showControllerFunction])
+
+function Router($stateProvider) {
+    $stateProvider
+        .state("welcome", {
+            url: "/",
+            templateUrl: "/assets/js/ng-views/welcome.html"
+        })
+        .state("index", {
+            url: "/recipes",
+            templateUrl: "/assets/js/ng-views/index.html",
+            controller: "indexController",
+            controllerAs: "vm"
+        })
+        .state("show", {
+            url: "/recipes/:recipe",
+            templateUrl: "/assets/js/ng-views/show.html",
+            controller: "showController",
+            controllerAs: "vm"
+        })
+}
+
+function Recipe ($resource) {
+    return $resource("/api/recipes/:recipe", {}, {
+    	update: {method: "PUT"}
+    })
+
+}
+function indexControllerFunction( Recipe ) {
+	this.recipes = Recipe.query()
+}
+
+function showControllerFunction( $stateParams, Recipe) {
+	this.recipe = Recipe.find($)
+}
